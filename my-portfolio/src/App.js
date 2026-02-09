167eac2f3141b76314f2e8cb73e605c299079b0d
ndhung23 <hatakezoxy@gmail.com>
2026-02-09 15:48:49 +0700

    Khung

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-primary text-center">Hello, đây là Portfolio của Hưng!</h1>
+      
+      <div className="card text-center mt-4">
+        <div className="card-header">
+          Test Bootstrap
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">Giao diện đã nhận Bootstrap</h5>
+          <p className="card-text">Nếu bạn thấy nút màu xanh bên dưới đẹp trai thì là thành công.</p>
+          <button className="btn btn-primary">Nút bấm Bootstrap</button>
+          <button className="btn btn-danger ms-2">Nút Cancel</button>
+        </div>
+      </div>
     </div>
   );
 }
